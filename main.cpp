26f1d1d51e5d9af9fbc18992e45cdc818d75c62f
Michelle Wiginton <michelle.wiginton00@gmail.com>
2026-06-16 08:09:54 -0500

    add one hard coded job

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
+
+struct Job {
+    std::string name;
+    std::string command;
+};
 
 int main() {
-    std::cout << "hello\n";
-    return 0;
+    const Job job{
+        "hello-job",
+        "echo Hello from the hardcoded job"
+    };
+
+    std::cout << "Running job: " << job.name << '\n';
+    std::cout << "Command: " << job.command << '\n';
+    std::cout << std::flush;
+
+    const int exit_code = std::system(job.command.c_str());
+
+    std::cout << "Job exited with code: " << exit_code << '\n';
+    return exit_code;
 }
